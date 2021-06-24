c1055f9ccf75eeb1217098c7aeb53fe86be397a6
zhangxiaze <zhangxiaze@live.com>
2021-06-24 11:03:57 +0800

    small improve

--- FileSystem.h
+++ FileSystem.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 
+
 #define FILE_CONTROL_BLOCK_SIZE 128u
 #define MAX_POINTER ((Super.BlockSize - sizeof(Block)) / sizeof(BlockIndex) + 10)
 #define MAX_BLOCK_SPACE (Super.BlockSize - sizeof(Block))
@@ -27,78 +28,80 @@ typedef int32_t FCBIndex;
  */
 
 class BiSet {
-  public:
-    BiSet(uint32_t size);
-    bool get(uint32_t index);
-    void set(uint32_t index, bool val);
-    ~BiSet();
-    uint32_t SizeOfBool;
-    uint32_t SizeOfByte;
-    uint8_t *data = nullptr;
+public:
+	BiSet(uint32_t size);
+	bool get(uint32_t index);
+	void set(uint32_t index, bool val);
+	~BiSet();
+	uint32_t SizeOfBool;
+	uint32_t SizeOfByte;
+	uint8_t* data = nullptr;
 };
 
-enum class Access : uint8_t {
-    None = 0,
-    Read = 1 << 0,
-    Write = 1 << 1,
-    Delete = 1 << 2,
+class Access {
+public:
+	static uint8_t None;
+	static uint8_t	Read;
+	static uint8_t	Write;
+	static uint8_t	Delete;
 };
+
 
 enum class FileType : uint8_t { File, Directory };
 //超级块（记录盘块信息）
 struct SuperBlock {
-    uint16_t FCS = 0; //循环校验码
+	uint16_t FCS = 0;              //循环校验码
 
-    uint32_t DiskSize;             //磁盘大小
-    uint32_t BlockSize;            //块大小
-    uint32_t BlockNum;             //块数量(=DISK_SIZE/块大小)
-    BlockIndex FCBBitmapOffset;    // FCB的位示图列表开始块号
-    BlockIndex DataBitmapOffset;   // Data的位示图数据块开始块号
-    BlockIndex FCBOffset;          // FCB列表开始块号
-    BlockIndex DataOffset;         // Data数据块开始块号
-    uint32_t FCBNum, DataBlockNum; // FCB数量和DataBlock数量
+	uint32_t DiskSize;             //磁盘大小
+	uint32_t BlockSize;            //块大小
+	uint32_t BlockNum;             //块数量(=DISK_SIZE/块大小)
+	BlockIndex FCBBitmapOffset;    // FCB的位示图列表开始块号
+	BlockIndex DataBitmapOffset;   // Data的位示图数据块开始块号
+	BlockIndex FCBOffset;          // FCB列表开始块号
+	BlockIndex DataOffset;         // Data数据块开始块号
+	uint32_t FCBNum, DataBlockNum; // FCB数量和DataBlock数量
 };
 
 // FCB的bitmap
-extern BiSet *FCBBitMap;
+extern BiSet* FCBBitMap;
 // Data数据块的bitmap
-extern BiSet *DataBitMap;
+extern BiSet* DataBitMap;
 
 struct Block {
-    uint16_t FCS = 0;  //循环校验码
-    uint16_t Size = 0; //块内有效数据的长度
+	uint16_t FCS = 0;  //循环校验码
+	uint16_t Size = 0; //块内有效数据的长度
 
-    // PointerBlock : BlockIndex[]
-    // Data		   : uint8_t[]
-    // Dir		   : FCBIndex[]
+	// PointerBlock : BlockIndex[]
+	// Data		   : uint8_t[]
+	// Dir		   : FCBIndex[]
 };
 
 struct DataBlock : Block {
-    uint8_t data[];
+	uint8_t data[];
 };
 struct PointerBlock : Block {
-    BlockIndex data[];
+	BlockIndex data[];
 };
 struct DirBlock : Block {
-    FCBIndex data[];
+	FCBIndex data[];
 };
 
 //文件控制段（一个Block里可以存放多个FCB）
 struct FileControlBlock {
-    FileControlBlock operator=(FileControlBlock &val);
-    FileControlBlock();
-    FileControlBlock(enum FileType t, const char *name, uint8_t AccessMode, FCBIndex parent);
+	FileControlBlock operator=(FileControlBlock& val);
+	FileControlBlock();
+	FileControlBlock(enum FileType t, const char* name, uint8_t AccessMode, FCBIndex parent);
 
-    char Name[32];
-    enum FileType Type;
-    uint32_t Size;
-    time_t CreateTime, ModifyTime, ReadTime;
-    uint8_t AccessMode;
-    FCBIndex Parent;
-    BlockIndex DirectBlock[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-    BlockIndex Pointer = -1;
+	char Name[32];
+	enum FileType Type;
+	uint32_t Size;
+	time_t CreateTime, ModifyTime, ReadTime;
+	uint8_t AccessMode;
+	FCBIndex Parent;
+	BlockIndex DirectBlock[10] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
+	BlockIndex Pointer = -1;
 
-    uint8_t __padding[6];
+	uint8_t __padding[6];
 };
 
 extern SuperBlock Super;
@@ -111,56 +114,23 @@ void PrintDiskInfo();
 
 void FormatDisk(uint32_t blocksize = 1 << 10, uint32_t FCBBlockNum = 0);
 
-/// <summary>
-/// 已废弃！创建新目录
-/// </summary>
-/// <param name="name">目录名</param>
-/// <param name="parent">父目录的FCB号</param>
-/// <returns>新建目录的FCB号，若创建失败则返回-1</returns>
+
 FCBIndex CreateDirectory(string name, FCBIndex parent);
-/// <summary>
-/// 已废弃！新建文件夹
-/// </summary>
-/// <param name="name">文件名</param>
-/// <param name="dir">父目录的FCB号</param>
-/// <returns>新建文件的FCB号，若创建失败则返回-1</returns>
+
 FCBIndex CreateFile(string name, FCBIndex dir);
 bool DeleteFile(FCBIndex file);
 void PrintDir(FCBIndex dir);
 void PrintFileInfo(FCBIndex file);
 
-/// <summary>
-/// 查询某目录下的文件FCB号
-/// </summary>
-/// <param name="dir">待查找目录</param>
-/// <param name="filename">待查找的文件名</param>
-/// <returns>查找到的FCB号，如果未查到或dir不是目录，则返回-1</returns>
+
 FCBIndex Find(FCBIndex dir, string filename);
 
-/// <summary>
-/// 读取文件
-/// </summary>
-/// <param name="file">文件FCB编号</param>
-/// <param name="pos">读取起始点</param>
-/// <param name="buff">缓冲区</param>
-/// <param name="len">读取长度</param>
-/// <returns>返回最后读取位置的下一位</returns>
-int64_t ReadFile(FCBIndex file, int64_t pos, int64_t len, uint8_t *buff);
-
-/// <summary>
-/// 写入文件
-/// </summary>
-/// <param name="file">文件FCB编号</param>
-/// <param name="pos">写入起始点</param>
-/// <param name="buff">缓冲区</param>
-/// <param name="len">写入长度</param>
-/// <returns>返回最后写入位置的下一位</returns>
-int64_t WriteFile(FCBIndex file, int64_t pos, int64_t len, uint8_t *buff);
-
-uint64_t FileInfo(FCBIndex file, FileControlBlock *fcb);
-
-void GetFCB(FCBIndex file, FileControlBlock *fcb);
-
 FCBIndex Create(string name, FCBIndex dir, enum FileType t);
+
+int64_t ReadFile(FCBIndex file, int64_t pos, int64_t len, uint8_t* buff);
+
+int64_t WriteFile(FCBIndex file, int64_t pos, int64_t len, const uint8_t* buff);
+
+void FileInfo(FCBIndex file, FileControlBlock* fcb);
 
 vector<FCBIndex> GetChildren(FCBIndex dir);
--- SimDisk.cpp
+++ SimDisk.cpp
@@ -7,62 +7,31 @@
 #include "FileSystem.h"
 using namespace std;
 
-inline uint16_t &xxx(uint8_t *blockBuff) { return ((Block *)blockBuff)->Size; }
+#define TestSize 3000
 
 int main() {
-    initDisk();
-    FormatDisk(1024);
-    auto dir1 = CreateDirectory("Dir1", 0);
-    auto dir2 = CreateDirectory("Dir2", 0);
-    auto dir3 = CreateDirectory("Dir3", dir2);
-    auto dir4 = CreateDirectory("Dir4", dir3);
-    CreateFile("File1", 0);
-    CreateFile("File2", 0);
-    CreateFile("File3", 0);
-    CreateFile("File4", 0);
-    CreateFile("File5", 0);
-    CreateFile("File6", 0);
-    CreateFile("File7", 0);
-    CreateFile("File8", 0);
-    auto data2 = CreateFile("Data2", dir1);
+	uint8_t testBuff[TestSize];
+	uint8_t testBuff2[TestSize];
+	ifstream fs("C:\\Users\\Azura\\Desktop\\VNS\\VNS - PURE.cpp");
+	fs.read((char*)testBuff, 10);
+	fs.read((char*)testBuff, TestSize);
 
-    // for (size_t i = 0; i < 5000; i++)
-    //{
-    //	CreateDirectory((string("Test") + to_string(i)).c_str(), 1);
-    //}
-    uint8_t *buff = (uint8_t *)malloc(2000);
 
-    PrintDiskInfo();
-    printf("\n\n");
-    PrintDir(0);
+	initDisk();
+	FormatDisk(1024);
+	auto dir1 = CreateDirectory("Dir1", 0);
+	auto dir2 = Create("Dir2", 0, FileType::Directory);
+	auto file1 = Create("File1", 0, FileType::File);
+	auto file2 = Create("File1", 0, FileType::File);
 
-    DeleteFile(data2);
+	WriteFile(file1, 0, 2000, testBuff);
 
-    PrintDir(0);
-    PrintDir(dir1);
-    printf("\n\n");
 
-    PrintDiskInfo();
-    printf("\n\n");
-    printf("\n\n");
-    printf("\n\n");
-    printf("\n\n");
-    printf("\n\n");
-    printf("\n\n");
-    auto file = CreateFile("TestFile", 0);
-    PrintFileInfo(file);
+	ReadFile(file1, 0, 2000, testBuff2);
 
-    WriteFile(file, 0, 2000, (uint8_t *)"hello world!");
-    WriteFile(file, 1010, 20, (uint8_t *)"hello world!");
-    PrintFileInfo(file);
+	PrintDir(0);
+	PrintDiskInfo();
+	PrintFileInfo(file1);
+	PrintFileInfo(0);
 
-    ReadFile(file, 1010, 20, buff);
-    printf("########################\n%s\n########################\n", buff);
-
-    // printf("pos = %lld\n", WriteFile(file, 0, 1021, (uint8_t*)"hello world!"));
-    // WriteFile(file, 0, 128, (uint8_t*)"hello world!");
-    PrintFileInfo(file);
-
-    PrintDir(0);
-    PrintDiskInfo();
 }
