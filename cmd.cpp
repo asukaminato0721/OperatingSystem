0a80558a1aded9a9bde1ca037c427cec82ea74b0
wyp <2217236949@qq.com>
2021-06-28 09:13:55 +0800

    Signed-off-by: wyp <2217236949@qq.com>

--- cmd.cpp
+++ cmd.cpp
@@ -1,4 +1,5 @@
 #include "FileSystem.h"
+#include "Driver.h"
 #include <bits/stdc++.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,12 +7,11 @@
 #include <string.h>
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 #define BLKSIZE 1024  // 数据块的大小
-#define BLKNUM Super.BlockNum    // 数据块的块数
 #define INODESIZE 128 // （fcb）i节点的大小
-#define INODENUM Super.FCBNum   // i节点的数目
 #define USERLEN 10          //设置用户名和密码最大长度
 
 typedef FileControlBlock Inode; // fcb别名Inode
@@ -28,7 +28,7 @@ char choice;
 vector<string> vc_of_str;
 string s1, s2;
 FCBIndex inum_cur;   //当前文件夹FCB号                                                                                                                                                   // 当前目录
-char temp[100 * BLKSIZE];  //写入缓冲区                                                                                                                                       // 缓冲区
+char temp[10 * BLKSIZE];  //写入缓冲区                                                                                                                                       // 缓冲区
 User user;     //用于缓存内存中的用户                                                                                                                                                 // 当前的用户
 //char bitmap[BLKNUM];                                                                                                                                            // 位图数组
 //Inode inode_array[INODENUM];                                                                                                                                    // i节点数组
@@ -514,7 +514,7 @@ void vi() {
     int i, inum;
     int64_t res;
     string temps1, temps2; int temp_cur;
-    char temp[100 * BLKSIZE];
+    char temp[10 * BLKSIZE];
     uint8_t* buff;
     if (s2.find('/') != -1) {  // 传入的不是文件名而是路径
         temps1 = s2.substr(0, s2.find_last_of('/') + 1);
@@ -839,6 +839,10 @@ void command(void)
 int main(void)
 {
     // format();
+    initDisk();
+    if (LoadDisk() == false) {
+        FormatDisk(4 * 1 << 10);
+    }
     login();
     init();
     command();
